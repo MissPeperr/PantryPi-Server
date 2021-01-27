061adac8734b04379a159bc4057d2e9f9f04fe81
Madi Peper <madisonpeper@gmail.com>
2021-01-27 16:53:22 -0600

    Added method for parsing the URL from the request

--- request_handler.py
+++ request_handler.py
@@ -3,6 +3,34 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class HandleRequests(BaseHTTPRequestHandler):
 
+    def parse_url(self, path):
+        path_params = path.split("/")
+        resource = path_params[1]
+
+        # setting up for query string params later
+        if "?" in resource:
+
+            param = resource.split("?")[1]
+            resource = resource.split("?")[0]
+            pair = param.split("=")
+            key = pair[0]
+            value = pair[1]
+
+            return (resource, key, value)
+
+        else:
+            id = None
+
+            try:
+                id = int(path_params[2])
+            except IndexError:
+                pass  
+            except ValueError:
+                pass  
+
+            return (resource, id)
+
+
     def _set_headers(self, status):
         self.send_response(status)
         self.send_header('Content-type', 'application/json')
