38df3ab06d689ce0d5b9320f06c470785b8f2f9a
Madi Peper <madisonpeper@gmail.com>
2021-01-26 23:03:39 -0600

    Initial commit;

---
+++ README.md
@@ -0,0 +1,12 @@
+# Pantry Pi
+
+This is a project I'm working on for scanning items into my pantry to keep track of.
+
+### Hardware
+- [Raspberry Pi 4](https://www.raspberrypi.org/products/raspberry-pi-4-model-b/)
+- [The Official Raspberry Pi Touchscreen Display](https://www.raspberrypi.org/products/raspberry-pi-touch-display/) (and the [SmartiPi Touch 2](https://smarticase.com/products/smartipi-touch-2) case)
+- A barcode [scanner](https://www.amazon.com/gp/product/B00LE5VV1C/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=piddlerinther-20&linkId=1ec48a209b97bed9ff01771bd109a58a&language=en_US)
+
+### Software
+- Python 3.8.1
+- SQLite3
---
+++ pantry.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Category (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `name` TEXT NOT NULL
+);
+
+CREATE TABLE Food (
+    `id` TEXT NOT NULL PRIMARY KEY,
+    `name` TEXT NOT NULL,
+    `quantity` INTEGER NOT NULL,
+    `barcode` TEXT NOT NULL,
+    `on_grocery_list` INTEGER NOT NULL,
+    `category_id` INTEGER NOT NULL,
+    FOREIGN KEY(`category_id`) REFERENCES `Category`(`id`)
+);
+
+INSERT INTO Category VALUES (null, "Sweets");
+INSERT INTO Category VALUES (null, "Snacks");
+INSERT INTO Category VALUES (null, "Breads");
+INSERT INTO Category VALUES (null, "Vegetables");
+
+INSERT INTO Food VALUES ("food_bn5ga70bfcre8eaft1zptb3ri979", "NABISCO, OREO, MILK'S FAVORITE SANDWICH COOKIES, CHOCOLATE", 1, "044000032029", 1, 1);
+INSERT INTO Food VALUES ("food_adq4r9la7hrqf2b3zqpmxbg73go2", "SANDWICH POTATO BREAD", 1, "075185007007", 0, 3);
+INSERT INTO Food VALUES ("food_bk64133bt6s1gwb2ltlfibzx8fxr", "ICE CREAM, VANILLA", 1, "853149008006", 0, 1);
+INSERT INTO Food VALUES ("food_bhgjeekag1cntnb3ty89ib5dgsf5", "SHARP WHITE CHEDDAR CHEESE", 2, "072830000956", 1, 1);
