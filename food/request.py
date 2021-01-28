954eec51bba04f1a45b7d9318dfb980f7f86f66a
Madi Peper <madisonpeper@gmail.com>
2021-01-27 18:28:09 -0600

    User can get all food for a particular category;

--- food/request.py
+++ food/request.py
@@ -30,3 +30,33 @@ def get_all_food():
             food_list.append(food.__dict__)
 
     return json.dumps(food_list)
+
+
+def get_food_by_category(cat_id):
+    with sqlite3.connect("./pantry.db") as conn:
+
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            f.id,
+            f.name,
+            f.quantity,
+            f.barcode,
+            f.on_grocery_list,
+            f.category_id
+        FROM Food f
+        WHERE f.category_id = ?
+        """, (cat_id,))
+
+        food_list = []
+
+        dataset = db_cursor.fetchall()
+            
+        for row in dataset:
+            food = Food(row['id'], row['name'], row['quantity'], row['barcode'], row['on_grocery_list'], row['category_id'])
+
+            food_list.append(food.__dict__)
+
+    return json.dumps(food_list)
